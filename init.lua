e2320dcd371ea8de307c1ae55ca843f6e505f3b3
ozoromo <36481318+ozoromo@users.noreply.github.com>
2023-11-03 15:35:04 +0100

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,8 @@
+return {
+  plugins = {
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.colorscheme.catppuccin" },
+    { import = "astrocommunity.completion.tabnine-nvim" },
+    { import = "astrocommunity.pack.rust" },
+    { import = "astrocommunity.pack.python" },
+}
